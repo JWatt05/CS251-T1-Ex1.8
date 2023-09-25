6bc2fecc2b8a45929c23066a4828dfa4f8fcfa30
wlb22171 <j.watt.2022@uni.strath.ac.uk>
2023-09-25 11:32:28 +0100

    (First Question) isMultiple method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       //System.out.println("Hello world!");
+        isMultiple();
     }
-}
+
+    private static long n = 7;
+    private static long m = 14;
+    private static boolean result;
+
+
+    public static void isMultiple(){
+
+        if (m % n == 0)
+        {
+            result = true;
+        }
+
+        else{
+            result = false;
+        }
+            System.out.println(result);
+        }
+
+    }
